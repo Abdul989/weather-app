ec36e91f12ed495a743dc1cd290c29d49cbd1f89
Abdul989 <abdulbaasitshariff@gmail.com>
2021-03-27 11:32:45 +0000

    Wind work

--- src/components/iphone/wind.js
+++ src/components/iphone/wind.js
@@ -17,7 +17,7 @@ export default class Wind extends Component {
 	// a constructor with initial set states
 	constructor(props){
 		super(props);
-        this.fetchForcastData()
+        this.fetchWindData()
         this.state = {
             selectValue: 0
           };
@@ -25,25 +25,25 @@ export default class Wind extends Component {
 		this.state.temp = "";
 		// button display state
 		this.setState({ display: true });
-        this.setState({AllWeatherData: ["Null"]});
         this.setState({fetchedWeather: false});
         this.setState({fetchedCalendar: false});
         this.setState({Home: false})
         this.setState({calendar: false})
         this.setState({map: false})
+        this.setState({stylingVar:null})
 
 
-        this.handleDropdownChange = this.handleDropdownChange.bind(this);
 
 	}
 
 
-	fetchForcastData = () => {
+	fetchWindData = () => {
+        console.log("Im fetching wind data")
 		var url = "http://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&APPID=bbe1b2a0cfe9d01b8ff8ceae42cc6ca1";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
-			success : this.parseCalendarResponse,
+			success : this.parseWindResponse,
 			error : function(req, err){ console.log('API call failed ' + err); }
 		})
 	}
@@ -69,22 +69,19 @@ export default class Wind extends Component {
 
 									<td><img src="../../assets/icons/location-icon.png" class={style.settings}></img></td>
 								</tr>
-							</table>
-                        </nav>
-                        <div class={style.title}>Wind Direction</div>
-                </div>
-                <div class={ style.conditions }>{ this.state.cond }</div>
-                <br></br>
-                <span class={ style.tempStyles }>{ this.state.temp }</span>
-                <div class={ style.details }>
-                    
-                    </div> 
-
-                    <div><button class={ style.wind} onClick={this.showHome} ><img class={ style.windbutton} src="../../assets/icons/home.png"/></button></div>
-					<div><button class={ style.calender} onClick={this.showCalendar} ><img class={ style.homebutton} src="../../assets/icons/wind.png"/></button></div>
-					<div><button class={ style.map} onClick={this.showMap} ><img class={ style.mapbutton} src="../../assets/icons/map.png"/></button></div>
+					</table>
+                    </nav>
+                <div class={style.title}>Wind Direction</div>
+            </div>
+            <div><img class={ this.state.stylingVar} src="../../assets/icons/arrowIcon.png"/></div>
+            <div class={ style.conditions }>{ this.state.windDegree, this.toTextualDescription() }</div>
+            <br></br>
+            <span class={ style.tempStyles }>{ Math.round(this.state.windSpeed)+ "mph" }</span>
+            <div><button class={ style.wind} onClick={this.showHome} ><img class={ style.windbutton} src="../../assets/icons/home.png"/></button></div>
+			<div><button class={ style.calender} onClick={this.showCalendar} ><img class={ style.homebutton} src="../../assets/icons/wind.png"/></button></div>
+			<div><button class={ style.map} onClick={this.showMap} ><img class={ style.mapbutton} src="../../assets/icons/map.png"/></button></div>
               	
-                </div>
+            </div>
 		);
             }else if(this.state.calender){
                 return(<div><Calendar/></div>);
@@ -97,11 +94,52 @@ export default class Wind extends Component {
             }
 	
         }
-        
-        
-        handleDropdownChange(e) {
-            this.setState({ selectValue: e.target.value });
+
+        //let theWindDeg =this.state.windDegree
+
+        toTextualDescription = () => {
+            //let stylingVar
+            /*
+            if (this.state.windDegree>337.5){
+                this.setState(stylingVar = style.arrowN)
+                
+                return 'Northerly';
+            } 
+            if (this.state.windDegree>292.5){ 
+                this.setState(stylingVar = style.arrowNW)  
+                return 'North Westerly';
+            }
+            if(this.state.windDegree>247.5){
+                this.setState({stylingVar: style.arrowW})  
+                console.log(this.state.stylingVar)
+                return 'Westerly';
+            } 
+            if(this.state.windDegree>202.5){
+                this.setState(stylingVar = style.arrowSW)
+                return 'South Westerly';
+            }
+            if(this.state.windDegree>157.5){ 
+                this.setState(stylingVar = style.arrowS)
+                return 'Southerly';
+            }
+            if(this.state.windDegree>122.5){ 
+                this.setState(stylingVar = style.arrowSE)
+                return 'South Easterly';
+            }
+            if(this.state.windDegree>67.5){
+                this.setState(stylingVar = style.arrowE)
+                return 'Easterly';
+            } 
+            if(this.state.windDegree>22.5){
+                this.setState(stylingVar = style.arrowNE)
+                return 'North Easterly';
+            }*/
+            return 'Northerly';
+            
         }
+        
+        
+        
           
         showHome = () => {
             this.setState({Home: true})
@@ -118,13 +156,14 @@ export default class Wind extends Component {
 
 
 
-        parseCalendarResponse = (parsed_json) => {
-            var AllWeatherData = parsed_json['list'];
-            var locate = parsed_json['city']['name'];
-            console.log(locate)
+        parseWindResponse = (parsed_json) => {
+            var windSpeed = parsed_json['list']['0']['wind']['speed'];
+            var windDegree = parsed_json['list']['0']['wind']['deg'];
+            console.log(windSpeed)
+            console.log(windDegree)
             this.setState({
-                location: locate,
-                AllWeatherData: AllWeatherData,
+                windSpeed: windSpeed,
+                windDegree: windDegree,
                 fetchedWeather: true,
                 fetchedCalendar: true
             });  
@@ -132,4 +171,5 @@ export default class Wind extends Component {
 
 
 
+        
 }
